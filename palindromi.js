1a41e38d86698f842d883def6382bb953f570ab9
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-18 18:47:17 +0200

    FIX pseudo code

--- palindromi.js
+++ palindromi.js
@@ -3,11 +3,13 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 // far inserire una parola all'utente
 
+const newWord = prompt("Inserisci una parola e scopri se è palindromo");
+
 // funzione
 // uniformare i caratteri della parola
 // rendere la parola speculare con reverse
 // confrontare se la parola sia identica al suo reverse
 // IF  true
-//  => RETURN la parola è palindroma
+//  => RETURN la parola è palindromo
 // ELSE
-//  => RETURN la parola non è palindroma
+//  => RETURN la parola non è palindromo
