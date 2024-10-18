bd91b3bd5fd76b48ebab1cce4e552f37e33261e3
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-18 18:46:18 +0200

    ADD palindromi pseudo code

---
+++ palindromi.js
@@ -0,0 +1,13 @@
+/* Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma */
+
+// far inserire una parola all'utente
+
+// funzione
+// uniformare i caratteri della parola
+// rendere la parola speculare con reverse
+// confrontare se la parola sia identica al suo reverse
+// IF  true
+//  => RETURN la parola è palindroma
+// ELSE
+//  => RETURN la parola non è palindroma
