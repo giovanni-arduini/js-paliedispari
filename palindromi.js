2ff417d75cf93f0bd8702cb8230cd54ff1abf673
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-18 19:11:32 +0200

    FIX comments

--- palindromi.js
+++ palindromi.js
@@ -12,23 +12,22 @@ function isPalindrome(word) {
   let message = " ";
   word = newWord;
   console.log(newWord);
+  // uniformare i caratteri della parola
   const wordUpperCases = word.toUpperCase();
   console.log(wordUpperCases);
-
+  // rendere la parola speculare con reverse
   const reversedWord = wordUpperCases.split("").reverse().join("");
   console.log(reversedWord);
+  // confrontare se la parola sia identica al suo reverse
+
+  // IF  true
+  //  => RETURN la parola è palindromo
   if (reversedWord === wordUpperCases) {
     return (message = "È palindromo!");
   }
+  // ELSE
+  //  => RETURN la parola non è palindromo
   return (message = "Non è palindromo");
 }
 
 console.log(isPalindrome());
-
-// uniformare i caratteri della parola
-// rendere la parola speculare con reverse
-// confrontare se la parola sia identica al suo reverse
-// IF  true
-//  => RETURN la parola è palindromo
-// ELSE
-//  => RETURN la parola non è palindromo
